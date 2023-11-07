f807723a2ddbf5932e92728b094f1e8ac11fd8a1
Jarred <jarred.muller@my.jcu.edu.au>
2023-11-07 10:58:41 +1000

    initial commit - update my details and framework for each function that needs to be defined

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,36 @@
 """
-Name:
-Date started:
-GitHub URL:
+Name: Jarred Muller
+Date started: 3/11/2023
+GitHub URL: https://github.com/cp1404-students/a1-Macro1989
 """
+import csv
 
 
 def main():
     """..."""
-    print("Song List 1.0 - by Your Name")
+    print("Song List 1.0 - by Jarred Muller")
+
+
+
+def create_song_list(songs_csv: str):
+
+
+    """ Creates a song list from a specified csv"""
+
+
+def display_menu():
+    """Displays a list of choices the users can make and returns their choice"""
+
+def display_song(song_list):
+    """ Display a list of all the songs with details and a count of these songs, unlearned songs will have an *"""
+
+
+def add_song():
+    """ prompts the user to add a song's title artist and year"""
+
+
+def complete_song():
+    """ prompts the user to add a song's title artist and year"""
 
 
 if __name__ == '__main__':
