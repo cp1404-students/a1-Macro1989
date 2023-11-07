f11db50b084d6fe9d66058eab46fb7c0c893ce76
Jarred <jarred.muller@my.jcu.edu.au>
2023-11-07 11:06:20 +1000

    display menu and song list functions first definitions and testing

--- assignment1.py
+++ assignment1.py
@@ -9,17 +9,36 @@ import csv
 def main():
     """..."""
     print("Song List 1.0 - by Jarred Muller")
-
+    song_list = create_song_list("songs.csv")
+    print(song_list)
 
 
 def create_song_list(songs_csv: str):
-
+    file = open(songs_csv)
+    csvreader = csv.reader(file)
+    song_list = []
+    for row in csvreader:
+        song_list.append(row)
+    file.close()
+    return song_list
 
     """ Creates a song list from a specified csv"""
 
 
 def display_menu():
     """Displays a list of choices the users can make and returns their choice"""
+    menu_choice = ""
+    while menu_choice != "D" and != "D" and != "D" and != "D":
+        print("""Menu:
+        D - Display songs
+        A - Add new song
+        C - Complete a song
+        Q - Quit""")
+        menu_choice = input().upper()
+        if menu_choice != "D" and != "D" and != "D" and != "D":
+            print("invalid choice")
+    return menu_choice
+
 
 def display_song(song_list):
     """ Display a list of all the songs with details and a count of these songs, unlearned songs will have an *"""
